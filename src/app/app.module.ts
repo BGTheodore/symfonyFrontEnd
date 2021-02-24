12ea52a4ca4f4966f7fe0caf99db171e1c0cae95
BGTheodore <bgtheodore18@gmail.com>
2021-02-24 15:54:00 -0500

    Continue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'movies', component: MovieListComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+
+const oktaConfig = {
+  issuer: 'https://dev-33095217.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oa90f29pDmMJMvpY5d6'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +26,8 @@ import { MovieListComponent } from './movie-list/movie-list.component';
     MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
