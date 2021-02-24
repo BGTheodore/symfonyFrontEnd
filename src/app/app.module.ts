41cec43a01f56d920c6f14686ce27c4788ea1a46
BGTheodore <bgtheodore18@gmail.com>
2021-02-24 16:06:43 -0500

    Continue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+import { Component } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import { HttpModule } from '@angular/http';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'movies', component: MovieListComponent },
+  { path: 'implicit/callback', component: OktaCallbackComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
@@ -19,6 +23,12 @@ const oktaConfig = {
   clientId: '0oa90f29pDmMJMvpY5d6'
 };
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,9 +37,35 @@ const oktaConfig = {
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    HttpModule,
+    RouterModule.forRoot(routes),
+    OktaAuthModule.initAuth(oktaConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+export class AppComponent {
+
+  isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService) {
+      this.oktaAuth.$authenticationState.subscribe(
+          (isAuthenticated: boolean) => (this.isAuthenticated = isAuthenticated)
+      );
+  }
+
+  async ngOnInit() {
+      this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  login() {
+      this.oktaAuth.loginRedirect('/');
+  }
+
+  logout() {
+      this.oktaAuth.logout('/');
+  }
+}
+
 export class AppModule { }
